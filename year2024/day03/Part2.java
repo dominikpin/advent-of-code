116342f98a5427a02ab3a8e47525ad999d9b0119
dominikpin <dp4550@student.uni-lj.si>
2024-12-03 10:47:34 +0100

    year2024 day03 completed

---
+++ year2024/day03/Part2.java
@@ -0,0 +1,37 @@
+package year2024.day03;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Part2 {
+    public Part2() throws FileNotFoundException {
+        File myObj = new File("year2024\\day03\\input.txt");
+        Scanner myReader = new Scanner(myObj);
+        String line = "";
+        while (myReader.hasNextLine()) {
+            line += myReader.nextLine();
+        }
+        int mulSum = parseInput(line);
+        myReader.close();
+        System.out.println(mulSum);
+    }
+
+    private static int parseInput(String line) {
+        String cleanedLine1 = line.replaceAll("don't\\(\\).*?do\\(\\)", "");
+        String cleanedLine2 = cleanedLine1.replaceAll("don't\\(\\).*", "");
+
+        Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+        Matcher matcher = pattern.matcher(cleanedLine2);
+        int mulSum = 0;
+        while (matcher.find()) {
+            int number1 = Integer.parseInt(matcher.group(1));
+            int number2 = Integer.parseInt(matcher.group(2));
+            mulSum += number1 * number2;
+        }
+
+        return mulSum;
+    }
+}
